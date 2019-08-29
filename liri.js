83659219b4c40b24e60490f8fc0a461d70bfecc6
Naj Dhillon <najdhil86@gmail.com>
2019-08-29 09:22:03 -0700

    Shifting to axios and movie portion

--- liri.js
+++ liri.js
@@ -4,9 +4,9 @@ require("dotenv").config();
 
 // console.log(spotify_id);
 
-var something = process.argv[2];
+var operation = process.argv[2];
 
-var song_name = process.argv[3];
+var user_input = process.argv[3];
 
 var keys = require("./keys.js");
 
@@ -14,38 +14,34 @@ var Spotify = require('node-spotify-api');
 
 var spotify = new Spotify(keys.spotify);
 
+var axios = require('axios');
 
 
-if (something == "concert-this") {
+
+if (operation == "concert-this") {
 
   console.log("hi")
 
-} else if (something == "spotify-this-song") {
+} else if (operation == "spotify-this-song") {
 
 
 
-  spotify.search({type: 'track', query: song_name}, function(err, data) {
+  spotify.search({type: 'track', query: user_input, limit: 20}, function(err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
     }
    
-    for (var i in data){
-
-      console.log(JSON.stringify(data.tracks.items[i]))
-
-
-    }
-    // console.log(JSON.stringify(data.tracks.items[0].album.artists[0].name))
-    // console.log(JSON.stringify(data.tracks.items[0].album.name)) 
+    console.log(JSON.stringify(data.tracks.items[0].album.artists[0].name))
+    console.log(JSON.stringify(data.tracks.items[0].album.name)) 
   });
 
 
     
-} else if (something == "movie-this") {
+} else if (operation == "movie-this") {
 
   console.log("hi")
     
-} else if (something == "do-what-it-says") {
+} else if (operation == "do-what-it-says") {
 
   console.log("hi")
     
