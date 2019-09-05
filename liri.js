f612269ca638784716ed661f45f4ad805c263779
Naj <najdhil86@gmail.com>
2019-09-04 22:36:16 -0700

    Adding functions for each command. In order to accomplish the last portion, I need to create functions for each command to order to convert what is read from a file to pass through the terminal

--- liri.js
+++ liri.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 
 var operation = process.argv[2];
 
-var user_input = process.argv[3];
+var user_input = process.argv.slice(3).join(" ");
 
 var keys = require("./keys.js");
 
@@ -27,97 +27,129 @@ var artist = process.argv.slice(3).join(" ");
 //Variable for look-for-it
 
 var fs = require("fs");
+function concert_this(a,b){
+  if (a == "concert-this") {
+    axios
+      .get(
+        `https://rest.bandsintown.com/artists/${b}/events?app_id=codingbootcamp`
+      )
+      .then(function(response) {
+        for (let i = 0; i < 100; i++) {
+          console.log(
+            "================================================================="
+          );
+          console.log(" ");
+  
+          console.log(response.data[i].venue.name);
+  
+          console.log(
+            response.data[i].venue.city + ", " + response.data[i].venue.country
+          );
+  
+          var concert_date = response.data[i].datetime;
+  
+          console.log(moment(concert_date).format("L"));
+  
+          console.log(" ");
+          console.log(
+            "================================================================="
+          );
+        }
+      })
+  
+      .catch(function(error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an object that comes back with details pertaining to the error that occurred.
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message);
+        }
+        console.log(error.config);
+      });
 
-if (operation == "concert-this") {
-  axios
-    .get(
-      `https://rest.bandsintown.com/artists/${process.argv
-        .slice(3)
-        .join(" ")}/events?app_id=codingbootcamp`
-    )
-    .then(function(response) {
+
+}
+
+}
+
+console.log(concert_this(operation,user_input))
+
+function spotify_this_song(a,b){
+  if (a == undefined){
+    spotify.search({ type: "track", query: "The Sign Ace of Base", limit: 20 }, function(
+      err,
+      data
+    ) {
+      if (err) {
+        return console.log("Error occurred: " + err);
+      }
+  
       for (let i = 0; i < 100; i++) {
         console.log(
           "================================================================="
         );
         console.log(" ");
-
-        console.log(response.data[i].venue.name);
-
+  
         console.log(
-          response.data[i].venue.city + ", " + response.data[i].venue.country
+          "Artist = " + JSON.stringify(data.tracks.items[i].album.artists[0].name)
         );
-
-        var concert_date = response.data[i].datetime;
-
-        console.log(moment(concert_date).format("L"));
-
+        console.log("Album:" + JSON.stringify(data.tracks.items[i].album.name));
+        console.log("Song: " + JSON.stringify(data.tracks.items[i].name));
+        console.log(
+          "Link: " + JSON.stringify(data.tracks.items[i].external_urls.spotify)
+        );
+  
         console.log(" ");
         console.log(
           "================================================================="
         );
       }
-    })
-
-    .catch(function(error) {
-      if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-      } else if (error.request) {
-        // The request was made but no response was received
-        // `error.request` is an object that comes back with details pertaining to the error that occurred.
-        console.log(error.request);
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.log("Error", error.message);
-      }
-      console.log(error.config);
     });
-} else if (operation == "spotify-this-song") {
-  spotify.search({ type: "track", query: user_input, limit: 20 }, function(
-    err,
-    data
-  ) {
-    if (err) {
-      return console.log("Error occurred: " + err);
-    }
+    
+  }
+  if (a == "spotify-this-song") {
+    spotify.search({ type: "track", query: b, limit: 20 }, function(
+      err,
+      data
+    ) {
+      if (err) {
+        return console.log("Error occurred: " + err);
+      }
+      for (let i = 0; i < 100; i++) {
+        console.log(
+          "================================================================="
+        );
+        console.log(" ");
+  
+        console.log(
+          "Artist = " + JSON.stringify(data.tracks.items[i].album.artists[0].name)
+        );
+        console.log("Album:" + JSON.stringify(data.tracks.items[i].album.name));
+        console.log("Song: " + JSON.stringify(data.tracks.items[i].name));
+        console.log(
+          "Link: " + JSON.stringify(data.tracks.items[i].external_urls.spotify)
+        );
+  
+        console.log(" ");
+        console.log(
+          "================================================================="
+        );
+      }
+    });
+  }
+}
 
-    // console.log(JSON.stringify(data.length));
+console.log(spotify_this_song(operation,user_input)) 
 
-    // console.log(
-    //   'Artist = ' + JSON.stringify(data.tracks.items[0].album.artists[0].name)
-    // )
-    // console.log('Album:' + JSON.stringify(data.tracks.items[0].album.name))
-    // console.log('Song: ' + JSON.stringify(data.tracks.items[0].name))
-    // console.log(
-    //   'Link: ' + JSON.stringify(data.tracks.items[0].external_urls.spotify)
-    // )
-
-    for (let i = 0; i < 100; i++) {
-      console.log(
-        "================================================================="
-      );
-      console.log(" ");
-
-      console.log(
-        "Artist = " + JSON.stringify(data.tracks.items[i].album.artists[0].name)
-      );
-      console.log("Album:" + JSON.stringify(data.tracks.items[i].album.name));
-      console.log("Song: " + JSON.stringify(data.tracks.items[i].name));
-      console.log(
-        "Link: " + JSON.stringify(data.tracks.items[i].external_urls.spotify)
-      );
-
-      console.log(" ");
-      console.log(
-        "================================================================="
-      );
-    }
-  });
-} else if (operation == "movie-this") {
+if (operation == "movie-this") {
   var movie = process.argv.slice(3).join(" ");
 
   axios
@@ -128,14 +160,16 @@ if (operation == "concert-this") {
     )
     .then(function(response) {
       if (process.argv[3] == undefined) {
-        user_input = "Mr.Nobody";
-        console.log(user_input);
-
         axios
           .get(
             "https://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&apikey=trilogy"
           )
           .then(function(response) {
+            console.log(
+              "================================================================="
+            );
+            console.log(" ");
+
             console.log("Title: " + response.data.Title);
 
             console.log("Year: " + response.data.Year);
@@ -159,8 +193,18 @@ if (operation == "concert-this") {
             console.log("Plot: " + response.data.Plot);
 
             console.log("Actors: " + response.data.Actors);
+
+            console.log(" ");
+            console.log(
+              "================================================================="
+            );
           });
       } else {
+        console.log(
+          "================================================================="
+        );
+        
+
         console.log("Title: " + response.data.Title);
 
         console.log("Year: " + response.data.Year);
@@ -183,17 +227,22 @@ if (operation == "concert-this") {
             break;
           }
         }
-
+        
         console.log("Country: " + response.data.Country);
-
+        
         console.log("Language: " + response.data.Language);
-
+        
         console.log("Plot: " + response.data.Plot);
-
+        
         console.log("Actors: " + response.data.Actors);
       }
-    })
+      console.log(" ");
 
+      console.log(
+        "================================================================="
+      );
+    })
+    
     .catch(function(error) {
       if (error.response) {
         // The request was made and the server responded with a status code
@@ -211,8 +260,13 @@ if (operation == "concert-this") {
       }
       console.log(error.config);
     });
-} else if (operation == "do-what-it-says") {
-  var contents = fs.readFileSync("./random.txt", "utf8");
+  } else if (operation == "do-what-it-says") {
+    var contents = fs.readFileSync("./random.txt", "utf8");
+    
+    var text_input = contents.split(',')
 
-  console.log(contents);
-}
+    process.argv[2] = text_input[0]
+    process.argv[3] = text_input[1]
+    
+  }
+  
