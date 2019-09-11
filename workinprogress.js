04c11416db99146b480b77ed95661c8148d5673a
Naj <najdhil86@gmail.com>
2019-09-10 21:24:42 -0700

    Working on bug fix for Concert_this

--- workinprogress.js
+++ workinprogress.js
@@ -18,46 +18,28 @@ var spotify = new Spotify(keys.spotify);
 //Concert This
 
 function concert_this(query) {
-  const output = [];
+  
     axios
       .get(
         `https://rest.bandsintown.com/artists/${query}/events?app_id=codingbootcamp`
       )
       .then(function(response) {
+        const concert_output = [];
+
         for (let i = 0; i < response.data.length; i++) {
         
-          output.push(response.data[i].venue.name);
+          concert_output.push(response.data[i].venue.name);
 
-          output.push(
+          concert_output.push(
             response.data[i].venue.city + ", " + response.data[i].venue.country
           );
 
           var concert_date = response.data[i].datetime;
 
-          output.push(moment(concert_date).format("L"));
+          concert_output.push(moment(concert_date).format("L"));
         }
-        
+        return concert_output
       })
-      
-      .catch(function(error) {
-        if (error.response) {
-          // The request was made and the server responded with a status code
-          // that falls out of the range of 2xx
-          console.log(error.response.data);
-          console.log(error.response.status);
-          console.log(error.response.headers);
-        } else if (error.request) {
-          // The request was made but no response was received
-          // `error.request` is an object that comes back with details pertaining to the error that occurred.
-          console.log(error.request);
-        } else {
-          // Something happened in setting up the request that triggered an Error
-          console.log("Error", error.message);
-        }
-        console.log(error.config);
-      });
-
-      return console.log(output.join('\n'))
 }
 
 //Testing the functionality of Concert This
